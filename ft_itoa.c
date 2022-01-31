ba26b2c77bbc6bf611b9eaff7b77fedb4e689e1b
Fiona Selanno <fcs_fiona@hotmail.com>
2022-01-31 23:57:26 +0100

    fix leaks

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,26 +6,32 @@
 /*   By: fiselann <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 16:00:33 by fiselann          #+#    #+#             */
-/*   Updated: 2022/01/25 16:05:00 by fiselann         ###   ########.fr       */
+/*   Updated: 2022/01/25 16:10:20 by fiselann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+#include <stdio.h>
 int	ft_len_result(int n)
 {
 	int	len;
-
 	len = 0;
 	if (n < 0)
 		len++;
 		n = -n;
-	while ((n /= 10))
+	if (n == 0)
 		len++;
-	return (len + 1);
-}*/
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	printf("len: %d\n", len);
+	return (len);
+}
 
-char	*ft_strcpy(char *dst, const char *src)
+//this function was only useful when  n == -2147483648. This is already taken care in ft_putnbr_itoa 
+/*char	*ft_strcpy(char *dst, const char *src)
 {
 	int	i;
 
@@ -37,33 +43,50 @@ char	*ft_strcpy(char *dst, const char *src)
 	}
 	dst[i] = '\0';
 	return (dst);
-}
+}*/
 
-char *ft_itoa(int n)
+int	ft_putnbr_itoa(unsigned int n, char *result, int i_result)
 {
-	char	*result;
-
-	result = malloc(sizeof(char) * 2); 
-	//only allocate space for 2 chars: the current char and '\0', Therefor ft_len_result(int); is not needed.
-	if (!result)
-		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(result, "-2147483648"));
-	if (n < 0)
+	if (n >= 10)
 	{
-		result[0] = '-';
-		result[1] = '\0';
-		n = -n;
-		result = ft_strjoin(result, ft_itoa(n));
-	}
-	else if (n >= 10)
-	{
-		result = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
+		i_result = ft_putnbr_itoa(n / 10, result, i_result);
+		i_result = ft_putnbr_itoa(n % 10, result, i_result);
 	}
 	else
 	{
-		result[0] = n + 48;
-		result[1] = '\0';
+		result[i_result] = (n + '0');
+		i_result++;
 	}
+	return (i_result);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+	int	i_result;
+
+	result = malloc(sizeof(*result) * ft_len_result(n) + 1);
+	if (!result)
+		return (NULL);
+	//next if condition is not needed anymore because the ft_putnbr_itoa takes an unisgned int
+	//if (n == -2147483648) 
+	//	return (ft_strcpy(result, "-2147483648"));
+	if (n < 0)
+	{
+		result[0] = '-';
+		n = -n;
+		i_result = ft_putnbr_itoa(n, result, 1); //start filling result at index 1
+	}
+	else
+		i_result = ft_putnbr_itoa(n, result, 0);
+	result[i_result] = '\0';
 	return (result);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("result: %s\n", ft_itoa(2147483648));
+}
+*/
